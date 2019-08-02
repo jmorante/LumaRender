85d2555c8f6dd2658e67b6910badb479dad33ef1
Jared Morante <jmorante@gmail.com>
2019-08-01 21:59:24 -0700

    Update main to hard code render a red sphere at the origin with radius 0.5. short vec3.h a bit.

--- vec3.h
+++ vec3.h
@@ -32,10 +32,8 @@ class vec3 {
         inline vec3& operator*=(const float t);
         inline vec3& operator/=(const float t);
 
-        inline float length() const {
-            return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
-        inline float squared_length() const {
-            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
+        inline float length() const { return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
+        inline float squared_length() const { return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
         inline void make_unit_vector();
 
         float e[3];
