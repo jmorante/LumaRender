bae77bc7532d2ae6157d83da0eb07fe7672b7c78
Jared Morante <jmorante@gmail.com>
2020-09-19 19:56:01 -0700

    Add camera.h class, integrate into main.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "hitable_list.h"
 #include "sphere.h"
 #include "float.h"
+#include "camera.h"
 
 float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
@@ -42,7 +43,10 @@ int main() {
     ppmFile.open("rgbFile.ppm");
     int nx = 600;
     int ny = 300;
+    int ns = 100;
+
     ppmFile << "P3\n" << nx << " " << ny << "\n255\n";
+    //std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
@@ -51,19 +55,26 @@ int main() {
     list[0] = new sphere(vec3(0,0,-1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
     hitable *world = new hitable_list(list, 2);
+    camera cam;
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i)/float(nx);
-            float v = float(j)/float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col(0,0,0);
+            for (int s=0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
 
-            vec3 p = r.point_at_paramter(2.0);
-            vec3 col = color(r, world);
-            //vec3 col = color(r);
+                vec3 p = r.point_at_paramter(2.0);
+                col += color(r, world);
+
+            }
+
+            col /= float(ns);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
             ppmFile << ir << " " << ig << " " << ib << "\n";
+            //std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
     ppmFile.close();
