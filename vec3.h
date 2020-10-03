df9774d0dd8d3a60168eb8b2d197671fcf0548a1
Jared Morante <jmorante@gmail.com>
2020-10-03 16:59:27 -0700

    Update vector definitions.

--- vec3.h
+++ vec3.h
@@ -48,111 +48,80 @@ class vec3 {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
 
+        inline static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+        inline static vec3 random(double min, double max) {
+            return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+        }
+
     public:
         double e[3];
 };
 
-using point3 = vec3; // 3D point
-using color = vec3;  // RGB color
+// vec3 Utility functions
 
-inline std::istream& operator>>(std::istream &is, vec3 &t) {
-    is >> t.e[0] >> t.e[1] >> t.e[2];
-    return is;
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
-    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
-    return os;
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline void vec3::make_unit_vector() {
-    float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
-    e[0] *= k; e[1] *= k; e[2] *= k;
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
-}
-
-inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
-}
-
-inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
-}
-
-inline vec3 operator*(float t, const vec3 &v) {
+inline vec3 operator*(double t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator/(vec3 v, float t) {
-    return vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
 }
 
-inline vec3 operator*(const vec3 &v, float t) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+inline vec3 operator/(vec3 v, double t) {
+    return (1/t) * v;
 }
 
-inline float dot(const vec3 &v1, const vec3 &v2) {
-    return v1.e[0] *v2.e[0] + v1.e[1] *v2.e[1]  + v1.e[2] *v2.e[2];
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0]
+           + u.e[1] * v.e[1]
+           + u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3 &v1, const vec3 &v2) {
-    return vec3( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
-                 (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
-                 (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
-}
-
-inline vec3& vec3::operator+=(const vec3 &v){
-    e[0]  += v.e[0];
-    e[1]  += v.e[1];
-    e[2]  += v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator*=(const vec3 &v){
-    e[0]  *= v.e[0];
-    e[1]  *= v.e[1];
-    e[2]  *= v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator/=(const vec3 &v){
-    e[0]  /= v.e[0];
-    e[1]  /= v.e[1];
-    e[2]  /= v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator-=(const vec3& v) {
-    e[0]  -= v.e[0];
-    e[1]  -= v.e[1];
-    e[2]  -= v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator*=(const float t) {
-    e[0]  *= t;
-    e[1]  *= t;
-    e[2]  *= t;
-    return *this;
-}
-
-inline vec3& vec3::operator/=(const float t) {
-    float k = 1.0/t;
-
-    e[0]  *= k;
-    e[1]  *= k;
-    e[2]  *= k;
-    return *this;
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+vec3 random_unit_vector() {
+    auto a = random_double(0, 2*pi);
+    auto z = random_double(-1, 1);
+    auto r = sqrt(1 - z*z);
+    return vec3(r*cos(a), r*sin(a), z);
+}
+
+using point3 = vec3; // 3D point
+using color = vec3;  // RGB color
+
 #endif //LUMARENDER_VEC3_H
