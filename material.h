ea479ed4b939c103877fcf3df350e6e69bef6ec3
Jared Morante <jmorante@gmail.com>
2020-10-03 17:25:44 -0700

    Add lambertian diffuse class to the material header.

--- material.h
+++ material.h
@@ -17,4 +17,21 @@ class material {
         ) const = 0;
 };
 
+class lambertian : public material {
+    public:
+        lambertian(const color& a) : albedo(a) {}
+
+        virtual bool scatter(
+                const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            vec3 scatter_direction = rec.normal + random_unit_vector();
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
+
+    public:
+        color albedo;
+};
+
 #endif //LUMARENDER_MATERIAL_H
