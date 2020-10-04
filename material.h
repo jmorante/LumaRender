c9f6cde243671f78ea4b24515d7ae3b85ab4c7bc
Jared Morante <jmorante@gmail.com>
2020-10-03 20:35:28 -0700

    Add the dielectric support to the materials header.

--- material.h
+++ material.h
@@ -52,4 +52,25 @@ class metal : public material {
         double fuzz;
 };
 
+class dielectric : public material {
+    public:
+        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attentuation, ray& scattered
+        ) const override {
+            attentuation = color(1.0, 1.0, 1.0);
+            double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+            vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+            scattered = ray(rec.p, refracted);
+            return true;
+        }
+
+    public:
+        double ir; // index of refraction
+};
+
 #endif //LUMARENDER_MATERIAL_H
