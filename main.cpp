0000af646b4d026085cc163f6fc9cc79fa8072c5
Jared Morante <jmorante@gmail.com>
2020-10-03 20:04:00 -0700

    Remove unused header files, add metal material support to some objects.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "raytracer.h"
 #include "hittable_list.h"
 #include "sphere.h"
-#include "float.h"
+#include "material.h"
 #include "camera.h"
 #include "color.h"
 #include "vec3.h"
@@ -57,8 +57,16 @@ int main() {
 
     // World
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3(0.0,-100.5,-1), 100, material_ground));
+    world.add(make_shared<sphere>(point3(0.0,0.0,-1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,0.0,-1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(1.0,000,-1.0), 0.5, material_right));
 
     // Camera
     auto viewport_height = 2.0;
